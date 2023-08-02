017be0e2809b6a22724dbb97cd668d2eccef9f69
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-02 10:27:15 +0530

    Hero Section completed.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navigation from './components/header/Navigation';
+import Hero from './components/header/UI/Hero';
 
 function App() {
   return (
     <div className="App">
       <>
       <Navigation />
+      <Hero />
       </>
     </div>
   );
