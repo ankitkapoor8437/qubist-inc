b9c4f58411a12e378259307077f2ea0bec8fddc3
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-02 13:51:39 +0530

    Team section completed.

---
+++ src/assets/data/teamDetails.js
@@ -0,0 +1,29 @@
+import team01 from '../images/team-01.png'
+import team02 from '../images/team-02.png'
+import team03 from '../images/team-03.png'
+import team04 from '../images/team-04.png'
+
+const teamData = [
+    {
+        imgUrl: team01,
+        name: "Ankit Kapoor",
+        position: "Product Developer"
+    },
+    {
+        imgUrl: team02,
+        name: "Ankit Kapoor",
+        position: "Product Developer"
+    },
+    {
+        imgUrl: team03,
+        name: "Ankit Kapoor",
+        position: "Product Developer"
+    },
+    {
+        imgUrl: team04,
+        name: "Ankit Kapoor",
+        position: "Product Developer"
+    },
+]
+
+export default teamData;
--- src/components/UI/Team.jsx
+++ src/components/UI/Team.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import '../styles/Team.css'
+import teamData from '../../assets/data/teamDetails'
 
 const Team = () => {
     return (
-        <div>Team</div>
+        <section className='our__team'>
+            <div className='container'>
+                <div className="team__content">
+                    <h6 className="subtitle">Our Team</h6>
+                    <h2>Meet with <span className="highlight">our team</span></h2>
+                </div>
+                <div className="team__wrapper">
+                    {
+                        teamData.map(({ imgUrl, name, position }) => (
+                            <div className="team__item">
+                                <div className="team__img">
+                                    <img src={imgUrl} alt="" />
+                                </div>
+                                <div className='team__details'>
+                                    <h4>{name}</h4>
+                                    <p className="description">{position}</p>
+
+                                    <div className="team__member-social">
+                                        <span>
+                                            <i className="ri-linkedin-line"></i>
+                                        </span>
+                                        <span>
+                                            <i className="ri-twitter-line"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
     )
 }
 
