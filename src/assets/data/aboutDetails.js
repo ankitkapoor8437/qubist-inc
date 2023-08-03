6724b548f9ada2580112d75104bb6a5b585d95f3
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-03 22:09:02 +0530

    Data changes! Bug Fix

--- src/assets/data/aboutDetails.js
+++ src/assets/data/aboutDetails.js
@@ -4,24 +4,21 @@ const aboutDetail = [
         title:'First working process',
         desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Dolorum non facere aspernatur, quia placeat molestiae asperiores
-        veritatis iste eveniet quis unde, sed animi qui iusto velit
-        mollitia reprehenderit eligendi similique?`
+        veritatis iste eveniet quis unde, sed animi qui iusto velit?`
     },
     {
         icon:'ri-team-line',
         title:'Dedicated team',
         desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Dolorum non facere aspernatur, quia placeat molestiae asperiores
-        veritatis iste eveniet quis unde, sed animi qui iusto velit
-        mollitia reprehenderit eligendi similique?`
+        veritatis iste eveniet quis unde, sed animi qui iusto velit?`
     },
     {
         icon:'ri-customer-service-2-line',
         title:'24/7 Hours support',
         desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Dolorum non facere aspernatur, quia placeat molestiae asperiores
-        veritatis iste eveniet quis unde, sed animi qui iusto velit
-        mollitia reprehenderit eligendi similique?`
+        veritatis iste eveniet quis unde, sed animi qui iusto velit`
     },
 
 ]
