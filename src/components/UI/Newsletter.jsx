476a4c0e75a07b5e4ae7736941a442cb7d4fb3c1
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-06 18:59:57 +0530

    Newsletter Section Completed.

---
+++ src/components/UI/Newsletter.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../styles/Newsletter.css'
+
+
+const Newsletter = () => {
+    return (
+        <section className='newsletter'>
+            <div className="container">
+                <div className="newsletter__wrapper">
+                    <div className="newsletter__content">
+                        <h6 className="subtitle">Let's work</h6>
+                        <h2>
+                            Explore the <span className='highlight'>hidden</span> ideas
+                            and
+                            subscribe!
+                        </h2>
+                    </div>
+
+                    <div className="newsletter__form">
+                        <input type="email" placeholder='Email' />
+                        <button className="secondary__btn">
+                            Subscribe Now
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Newsletter;
