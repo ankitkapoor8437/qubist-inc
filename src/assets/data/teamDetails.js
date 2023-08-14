08ceaad5df07e8a7fc84985196422812c52c94b7
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-14 10:19:56 +0530

    Bug fix!

--- src/assets/data/teamDetails.js
+++ src/assets/data/teamDetails.js
@@ -1,7 +1,7 @@
 import team01 from '../images/team-01.png'
 import team02 from '../images/team-02.png'
 import team03 from '../images/team-03.png'
-import team04 from '../images/team-04.png'
+// import team04 from '../images/team-04.png'
 
 const teamData = [
     {
